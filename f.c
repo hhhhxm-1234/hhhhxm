a40648a99f090d80d0a6349e62e337b68af875c6
hhhhxm-1234 <hxm004218@qq.com>
2025-07-23 16:21:52 +0800

    f.c commit

---
+++ f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("first push files success1\n");
+	printf("first push files success2\n");
+	printf("first push files success3\n");
+	printf("first push files success4\n");
+	printf("first push files success5\n");
+
+	return 0;
+}
