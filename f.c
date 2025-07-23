2f1617e6377c04e18e65fbb3026871151d11edc7
hhhhxm-1234 <hxm004218@qq.com>
2025-07-23 16:36:03 +0800

    插入修改

--- f.c
+++ f.c
@@ -4,11 +4,7 @@
 
 int main(void)
 {
-	printf("first push files success1\n");
-	printf("first push files success2\n");
-	printf("first push files success3\n");
-	printf("first push files success4\n");
-	printf("first push files success5\n");
+	printf("first push files success\n");
 
 	return 0;
 }
